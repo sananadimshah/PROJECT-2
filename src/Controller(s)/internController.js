7999ff5633bdcbe94386d0f8bee549eff708c68e
Adesh Singh Rajawat <aadeshsingh1947@gmail.com>
2022-09-14 16:21:52 +0530

    done intern validation

--- src/Controller(s)/internController.js
+++ src/Controller(s)/internController.js
@@ -4,19 +4,48 @@ const internModel = require("../model(S)/internModel")
 
 
 const createIntern= async (req,res)=>{
-    try{
-        let data = req.body
-        let {name,email,mobile,collegeName}= data
-        let collegeDetails= await collegeModel.findOne({collegeName:data.collegeName})
-        let collegeId= collegeDetails._id.toString()
-        const myData = {name,mobile,collegeId,email} 
-        let savedata= await internModel.create(myData)
-        res.status(200).send({status:true, data:savedata})
-    }
-    catch (err){ 
-        res.status(500).send({status:false, msg: err.message})
-    }
+ try{
+ let data = req.body
+    let {name,email,mobile,collegeName}= data
+
+   if(Object.keys(req.body).length==0){
+          res.status(400).send({status:false, msg:"please insert correct intern details"})
+   }    
+     if(!name) return res.status(400).send({status:false, msg:"please input name"})
+    //  let validName= await internModel.findOne({name:data.name})
+    //  if(validName) return res.status(403).send({status:false,msg: "BRO!! This name is allready taken"})
+     if(typeof(name)!=="string") return res.status(400).send({status:false, msg:"please input valid name"})
+     
+     if(typeof(email)!=="string") return res.status(400).send({status:false, msg: "please input correct email"})
+
+ if(!email) return res.status(400).send({status:false, msg:"please input email"})
+ let emaiId = await internModel.findOne({email:data.email})
+
+ if(emaiId) return res.status(401).send({status: false,msg: "please input another e-mailId" })
+
+    if(!mobile) return res.status(400).send({status:false, msg: "please input mobile number"})
+    if(typeof(mobile)!== Number)  return res.status(400).send({status:false, msg:"please input valid number"})
+    let validMobileNo= await internModel.findOne({mobile:data.mobile})
+    if(validMobileNo) return res.status(403).send({status:false , msg: "Sir, Please input valid mobile number"})
+
+    if(!collegeName) return res.status(400).send({status:false, msg:"please input collegeName"})
+    
+    
+    //  if()
+        
+  let collegeDetails= await collegeModel.findOne({collegeName:data.collegeName})
+
+    let collegeId= collegeDetails._id.toString()
+
+  const myData = {name,mobile,collegeId,email} 
+ let savedata= await internModel.create(myData)
+  res.status(200).send({status:true, data:savedata})
 }
+  
+  catch (err){ 
+        res.status(500).send({status:false, msg: err.message})
+  }
+  }
    
 
 
@@ -26,17 +55,6 @@ const createIntern= async (req,res)=>{
 
 
 
-//     let {name,email,mobile,collegeName} = req.body
-//     if(Object.keys(req.body).length==0){
-//         res.status(400).send({status:false, msg:"please insert correct intern details"})
-//     }
-//     if(!name) return res.status(400).send({status:false, msg:"please input name"})
-//     if(!email) return res.status(400).send({status:false, msg:"please input email"})
-//     if(!mobile) return res.status(400).send({status:false, msg: "please input mobile number"})
-//     if(!collegeName) return res.status(400).send({status:false, msg:"please input collegeName"})
-
-//     if(typeof())
-// }
 
 
 
